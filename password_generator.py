cc551096644089e58e2e5d74fac9ab931a1b4833
maviakhattak31-png <maviakhattak31@gmail.com>
2026-07-11 11:58:53 +0500

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,9 +1,15 @@
-import random  
-import string 
+import random
+import string
 
-pass_len=8
-character_val= string.ascii_letters + string.digits + string.punctuation
+pass_len=int(input("password length:"))
+
+symbol_choice=input("include symbols?(yes/no):")
+if symbol_choice == "yes":
+    character_val=string.ascii_letters + string.digits + string.punctuation
+else:
+    character_val = string.ascii_letters
+        
 password=""
 for i in range(pass_len):
     password+=random.choice(character_val)
-print("my password is:",password)
+print("your password is:",password)
